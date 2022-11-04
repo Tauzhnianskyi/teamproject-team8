4af63d1fd67251ccfa17e2e136368467982d317c
Tauzhnianskyi <104843369+Tauzhnianskyi@users.noreply.github.com>
2022-11-04 18:34:54 +0000

    Deploying to gh-pages from @ Tauzhnianskyi/teamproject-team8@d49b2b0db8a521b3b06204a88b37a5d89c7f8c3f 🚀

--- pages/library.b7bd2ae6.js
+++
@@ -1,2 +0,0 @@
-!function(){let a;a=window.location.href.includes("library")?"library":"home",document.querySelector(`[data-${a}]`).classList.add("nav-link__active")}();
-//# sourceMappingURL=library.b7bd2ae6.js.map
--- pages/library.fa509f57.js
+++
@@ -1,2 +0,0 @@
-!function(){let a;a=window.location.href.includes("library")?"library":"home",document.querySelector(`[data-${a}]`).classList.add("nav-link__active")}();
-//# sourceMappingURL=library.fa509f57.js.map
