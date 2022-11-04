510f08fe4fbc7a6c32d6cc8dd167975fd7ce91c1
Tauzhnianskyi <104843369+Tauzhnianskyi@users.noreply.github.com>
2022-11-04 07:17:50 +0000

    Deploying to gh-pages from @ Tauzhnianskyi/teamproject-team8@7b356c2874ba09ddc7cf4b158188964229ccc03a 🚀

---
+++ pages/library.fa509f57.js
@@ -0,0 +1,2 @@
+!function(){let a;a=window.location.href.includes("library")?"library":"home",document.querySelector(`[data-${a}]`).classList.add("nav-link__active")}();
+//# sourceMappingURL=library.fa509f57.js.map
